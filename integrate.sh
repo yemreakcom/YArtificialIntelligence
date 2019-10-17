b953b897f341822f2f33e62e7d134f21911473dc
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-10-17 12:13:20 +0300

    🐞

--- integrate.sh
+++ integrate.sh
@@ -1,4 +1,4 @@
-python ../YGitBookIntegration/integrate.py . -ll 1 -l ../YWiki/Yapay\ Zeka/README.md -u https://ai.yemreak.com
+python ../YGitBookIntegration/integrate.py . -l ../YWiki/Yapay\ Zeka/README.md -u https://ai.yemreak.com
 echo "---
 description: Sitede neler olup bittiğinin raporudur.
 ---" > CHANGELOG.md
